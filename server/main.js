4ecb573a3a6650ce62b3c83c53cb83a5933e0621
DonCabin <joaodequadros@hotmail.com>
2019-10-25 12:08:10 -0400

    Login Facebook Working!

--- server/main.js
+++ server/main.js
@@ -5,3 +5,12 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+ServiceConfiguration.configurations.remove({
+  service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+    service: "facebook",
+    appId: '2606632389429669',
+    secret: '5a4cc8cb1b6614654909719992baead5'
+});
